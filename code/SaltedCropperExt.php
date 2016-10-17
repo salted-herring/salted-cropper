703d47bda61eb8bca02521959378ab3b8c8973a0
Leo Chen <leo@saltedherring.com>
2016-10-17 10:33:28 -1200

    bugfix

--- code/SaltedCropperExt.php
+++ code/SaltedCropperExt.php
@@ -4,7 +4,7 @@ class SaltedCropperExt extends DataExtension {
 	protected static $db = array(
 		'SaltedCropperScheduled'	=>	'Boolean'
 	);
-	
+
 	public function updateCMSFields( FieldList $fields ) {
 		$fields->addFieldsToTab(
 			'Root.Main',
@@ -13,23 +13,23 @@ class SaltedCropperExt extends DataExtension {
 			)
 		);
 	}
-	
+
 	public function onBeforeWrite() {
 		parent::onBeforeWrite();
 		if (Controller::curr() && $this->owner->SaltedCropperScheduled) {
-			
+
 			//Debugger::inspect('here', false);
 			$request = Controller::curr()->request;
 			if ($bindings = $request->postVar('cropper_bindings')) {
-				
+
 				//Debugger::inspect($request->postVars());
-				
+
 				foreach($bindings as $binding) {
 					$chain = explode(':', $binding);
 					$source = $chain[0];
 					$target = $chain[1];
 					$source = $this->owner->$source();
-					
+
 					$canvas_x = $request->postVar($target . '_salted_container_x');
 					$canvas_y = $request->postVar($target . '_salted_container_y');
 					$canvas_w = $request->postVar($target . '_salted_container_width');
@@ -38,21 +38,21 @@ class SaltedCropperExt extends DataExtension {
 					$cropper_y = $request->postVar($target . '_salted_cropper_y');
 					$cropper_w = $request->postVar($target . '_salted_cropper_width');
 					$cropper_h = $request->postVar($target . '_salted_cropper_height');
-					
+
 					if ($this->owner->$target()->exists()) {
 						$this->owner->$target()->delete();
 					}
-					
+
 					$target = $target . 'ID';
-					
+
 					$this->cropImage($source, $target, $canvas_x, $canvas_y, $canvas_w, $canvas_h, $cropper_x, $cropper_y, $cropper_w, $cropper_h);
 				}
-				
+
 				$this->owner->SaltedCropperScheduled = false;
 			}
 		}
 	}
-	
+
 	private function cropImage($source, $target, $canvas_x, $canvas_y, $canvas_w, $canvas_h, $cropper_x, $cropper_y, $cropper_w, $cropper_h) {
 		$cropped = $this->duplicateImage($source);
 		$cropped->write();
@@ -64,9 +64,9 @@ class SaltedCropperExt extends DataExtension {
 			$this->scaleCropGD($cropped->getFullPath(), $canvas_x, $canvas_y, $canvas_w, $canvas_h, $cropper_x, $cropper_y, $cropper_w, $cropper_h);
 		}
 	}
-	
+
 	private function scaleCropImagick($image_path, $canvas_x, $canvas_y, $canvas_w, $canvas_h, $cropper_x, $cropper_y, $cropper_w, $cropper_h) {
-		
+
 		$imagick = new Imagick($image_path);
 		$original_width = $imagick->getImageWidth();
 		$x = $cropper_x + $canvas_x;
@@ -78,11 +78,11 @@ class SaltedCropperExt extends DataExtension {
 			$x = $x * $ratio;
 			$y = $y * $ratio;
 		}
-		$imagick->cropImage ( $cropper_w , $cropper_h , $x , $y );
+		$imagick->cropImage( $cropper_w , $cropper_h , $x , $y );
 		$imagick->writeImage($image_path);
 		$imagick->destroy();
 	}
-	
+
 	private function scaleCropGD($image_path, $canvas_x, $canvas_y, $canvas_w, $canvas_h, $cropper_x, $cropper_y, $cropper_w, $cropper_h) {
 		$imgInfo = getimagesize($image_path);
 		switch ($imgInfo[2]) {
@@ -98,8 +98,8 @@ class SaltedCropperExt extends DataExtension {
 			default:
 				return false;
 		}
-		
-		$original_width = ImageSX($image_path);
+
+		$original_width = ImageSX($image);
 		$x = $cropper_x + $canvas_x;
 		$y = $cropper_y + $canvas_y;
 		if ($original_width != $canvas_w) {
@@ -109,17 +109,17 @@ class SaltedCropperExt extends DataExtension {
 			$x = $x * $ratio;
 			$y = $y * $ratio;
 		}
-		
+
 		$newImg = imagecreatetruecolor($cropper_w, $cropper_h);
-		
-		imagecopyresampled($newImg, $image, 0, 0, $x, $y, $cropper_w, $cropper_h, $imgInfo[0], $imgInfo[1]);
+
+		imagecopyresampled($newImg, $image, 0, 0, $x, $y, $cropper_w, $cropper_h, $cropper_w, $cropper_h);
 		imagejpeg($newImg, $image_path, 100);
 		imagedestroy($newImg);
 	}
-	
-	
+
+
 	public static function scaleCrop($img, $w, $h, $ww, $hh, $x = 0, $y = 0, $flattened_dest = null, $level = null) {
-		
+
 		$imgInfo = getimagesize($img);
 		$fn = $img;
 		switch ($imgInfo[2]) {
@@ -135,7 +135,7 @@ class SaltedCropperExt extends DataExtension {
 			default:
 				return false;
 		}
-		
+
 		$newImg = imagecreatetruecolor($w, $h);
 		/* Check if this image is PNG or GIF, then set if Transparent */
 		if (($imgInfo[2] == 1) || ($imgInfo[2] == 3)) {
@@ -144,79 +144,79 @@ class SaltedCropperExt extends DataExtension {
 			$transparent = imagecolorallocatealpha($newImg, 255, 255, 255, 127);
 			imagefilledrectangle($newImg, 0, 0, $w, $h, $transparent);
 		}
-		
+
 		imagecopyresampled($newImg, $image, 0, 0, 0, 0, $w, $h, $imgInfo[0], $imgInfo[1]);
-		
+
 		$newImg_crop = imagecreatetruecolor($ww, $hh);
 		$flattened_crop = imagecreatetruecolor($ww, $hh);
 		imagecopy($newImg_crop, $newImg, 0, 0, $x, $y, $w, $h);
 		imagecopy($flattened_crop, $newImg, 0, 0, $x, $y, $w, $h);
-		
+
 		$eyePatchPath = $_SERVER['DOCUMENT_ROOT']. '/themes/default/images/white_triangle_png8.png';
 		$eyePatchInfo = getimagesize($eyePatchPath);
 		$eyeWidth = $eyePatchInfo[0];
 		$eyeHeight = $eyePatchInfo[1];
 		$dst_y = ($hh - $eyeHeight) * 0.5;
-		
+
 		$eyePatch = imagecreatefrompng($eyePatchPath);
 		imagealphablending($eyePatch, true);
 		imagesavealpha($newImg_crop, false);
 		imagecopy($newImg_crop, $eyePatch, 0, $dst_y, 0, 0, $eyeWidth, $eyeHeight);
-		
+
 		if (!is_null($level)) {
 			$LevelPatchInfo = getimagesize($level);
 			$LevelPatch = imagecreatefrompng($level);
-			
+
 			$newEyes = imagecreatetruecolor(200, 100);
 			imagealphablending($newEyes, false);
 			imagesavealpha($newEyes, true);
 			$transparent = imagecolorallocatealpha($newEyes, 255, 255, 255, 127);
 			imagefilledrectangle($newEyes, 0, 0, 200, 100, $transparent);
-			
+
 			imagecopyresampled($newEyes, $LevelPatch, 0, 0, 0, 0, 200, 100, $LevelPatchInfo[0], $LevelPatchInfo[1]);
 			imagealphablending($newEyes, true);
 			imagesavealpha($flattened_crop, false);
 			imagecopy($flattened_crop, $newEyes, 0, $dst_y, 0, 0, 200, 100);
-			
+
 			imagejpeg($flattened_crop, $flattened_dest, 91);
 			imagedestroy($flattened_crop);
 			imagedestroy($newEyes);
 		}
-			
+
 
 		imagejpeg($newImg_crop, $fn, 91);
 		imagedestroy($newImg);
 		imagedestroy($newImg_crop);
 		imagedestroy($eyePatch);
-		
+
 		return true;
-		
+
 	}
-	
-	
+
+
 	private function duplicateImage($src_image) {
 		$currenFolder = str_replace('assets/', '', $src_image->Parent()->getRelativePath());
 		$folder = Folder::find_or_make($currenFolder.'cropper_resamples');
 		$dest_image = new Image();
 		$newFileName = ltrim($this->getUniqueFileName($folder->getRelativePath(), $src_image->Name, $src_image->ID, $folder),'_');
 		$newFileName = strtolower(str_replace('_','-', $newFileName));
-		
+
 		copy($src_image->getFullPath(), $folder->getFullPath().$newFileName);
-		
+
 		$dest_image->setName($newFileName);
 		$dest_image->setParentID($folder->ID);
 		return $dest_image;
 	}
-	
+
 	private function getUniqueFileName($path, $fileName, $fileID, $parentFolder) {
 		$pathinfo = pathinfo($fileName);
 		$file = File::get()->filter(array(
 			'Name' 		=> $fileName,
 			'ParentID'	=> $parentFolder->ID
 		))->exclude('ID', $fileID);
-		
+
 		$i = 1;
-		
+
 		while ($file->count() != 0) {
 			$fileName = sprintf('%s.%d.%s', $pathinfo['filename'], $i++, $pathinfo['extension']);
 			$file = File::get()->filter(array(
@@ -224,7 +224,7 @@ class SaltedCropperExt extends DataExtension {
 				'ParentID'	=> $parentFolder->ID
 			))->exclude('ID', $fileID);
 		}
-		
+
 		return $fileName;
 	}
 }
--- code/SaltedCropperField.php
+++ code/SaltedCropperField.php
@@ -1,9 +1,11 @@
 <?php
+
 use SaltedHerring\Debugger as Debugger;
+
 class SaltedCropperField extends LiteralField {
-	
-	public function __construct($name, $source, &$object, $cropper_ratio = 1) {
-		
+
+	public function __construct($name, $source, $object, $cropper_ratio = 1) {
+
 		$source_image = $object->$source();
 		$width = $source_image->Width;
 		$height = $source_image->Height;
@@ -12,25 +14,25 @@ class SaltedCropperField extends LiteralField {
 		$calc_height = $height * $ratio;
 		$styles = " style=\"width:{$calc_width}px; height:{$calc_height}px\"";
 		parent::__construct(
-			$name, 
+			$name,
 			($object->$name()->exists() ? ('<div id="salted-cropped-'. $name .'" style="max-width: 700px;"><img style="max-width: 100%; height: auto;" src="'.$object->$name()->URL.'" width="" height="" /></div>') : '<h3 id="salted-cropped-'. $name .'">- haven\'t cropped -</h3>').
 			($source_image->exists() ? '<div class="salted-cropper"'. $styles .' data-source="'.$source.'" data-name="'.$name.'" data-cropper-ratio="'.$cropper_ratio.'" data-min-width="'. $calc_width .'" data-min-height="' . $calc_height . '"><img src="'.$source_image->URL.'" width="'.$width.'" height="'.$height.'" /></div>' : '')
 		);
-		
+
 		Requirements::combine_files(
-			'cropperfield-all.css', 
+			'cropperfield-all.css',
 			array(
 				SALTEDCROPPER_PATH . '/js/cropperjs/dist/cropper.min.css'
 			)
 		);
-		
+
 		Requirements::combine_files(
-			'cropperfield-all.js', 
+			'cropperfield-all.js',
 			array(
-				SALTEDCROPPER_PATH . '/js/cropperjs/dist/cropper.min.js', 
+				SALTEDCROPPER_PATH . '/js/cropperjs/dist/cropper.min.js',
 				SALTEDCROPPER_PATH . '/js/salted-cropper.js'
 			)
 		);
 	}
-	
 }
+
