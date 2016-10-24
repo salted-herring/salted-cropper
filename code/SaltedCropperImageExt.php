513f6456a168e74899afccc58366feaaa1422eb2
Leo Chen <leo@saltedherring.com>
2016-10-23 18:56:30 -1200

    extending image

--- code/SaltedCropperImageExt.php
+++ code/SaltedCropperImageExt.php
@@ -1,8 +1,20 @@
 <?php
 
 class SaltedCropperImageExt extends DataExtension {
+	
+	protected static $db = array(
+		'ContainerX'			=>	'Int',
+		'ContainerY'			=>	'Int',
+		'ContainerWidth'		=>	'Int',
+		'ContainerHeight'		=>	'Int',
+		'CropperX'				=>	'Int',
+		'CropperY'				=>	'Int',
+		'CropperWidth'			=>	'Int',
+		'CropperHeight'			=>	'Int'
+	);
+	
     protected static $has_one = array(
-        'SaltedCroppedImage'  =>  'Image'
+        'SaltedCroppedImage'	=>	'Image'
     );
 
     public function onBeforeDelete() {
