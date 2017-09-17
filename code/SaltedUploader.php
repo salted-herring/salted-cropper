0b50b4e4177bb7bf280f04469644c1f92eb1b6d8
Leo Chen <leo@saltedherring.com>
2017-09-16 20:19:07 -1200

    issue#2

--- code/SaltedUploader.php
+++ code/SaltedUploader.php
@@ -1,18 +1,22 @@
 <?php
 
-class SaltedUploader extends UploadField {
-    private $Ratio = 1;
-    public function __construct($name, $title = null, SS_List $items = null) {
+class SaltedUploader extends UploadField
+{
+    private $Ratio          =   null;
+
+    public function __construct($name, $title = null, SS_List $items = null)
+    {
         parent::__construct($name, $title);
     }
 
-    public function setCropperRatio($ratio) {
-        $this->Ratio = $ratio;
+    public function setCropperRatio($ratio)
+    {
+        $this->Ratio        =   $ratio;
         return $this;
     }
 
-    public function getFileEditFields(File $file) {
-        
+    public function getFileEditFields(File $file)
+    {
         Requirements::combine_files(
             'cropperfield-all.css',
             array(
@@ -30,27 +34,27 @@ class SaltedUploader extends UploadField {
 
         $fields = parent::getFileEditFields($file);
         if ($file->ClassName == 'Image') {
-            $name = $this->name;
-    		$width = $file->Width;
-    		$height = $file->Height;
-    		$ratio = $width > 666 ? (666 / $width) : 1;
-    		$calc_width = $width * $ratio;
-    		$calc_height = $height * $ratio;
-    		$styles = " style=\"width:{$calc_width}px; height:{$calc_height}px\"";
+            $name           =   $this->name;
+            $width          =   $file->Width;
+            $height         =   $file->Height;
+            $ratio          =   $width > 666 ? (666 / $width) : 1;
+            $calc_width     =   $width * $ratio;
+            $calc_height    =   $height * $ratio;
+            $styles         =   " style=\"width:{$calc_width}px; height:{$calc_height}px\"";
 
-            $html = '<div class="salted-cropper"'. $styles .' data-name="'.$name.'" data-cropper-ratio="'.$this->Ratio.'" data-min-width="'. $calc_width .'" data-min-height="' . $calc_height . '"><img src="'.$file->URL.'" width="'.$width.'" height="'.$height.'" /></div>';
+            $html           =   '<div class="salted-cropper"'. $styles .' data-name="'.$name.'" data-cropper-ratio="'.$this->Ratio.'" data-min-width="'. $calc_width .'" data-min-height="' . $calc_height . '"><img src="'.$file->URL.'" width="'.$width.'" height="'.$height.'" /></div>';
 
             $fields->push(LiteralField::create('SaltedCropper', $html));
             $fields->push(HiddenField::create('CropperRatio')->setValue($this->Ratio));
             $fields->push(HiddenField::create('ContainerX'));
             $fields->push(HiddenField::create('ContainerX'));
-    		$fields->push(HiddenField::create('ContainerY'));
-    		$fields->push(HiddenField::create('ContainerWidth'));
-    		$fields->push(HiddenField::create('ContainerHeight'));
-    		$fields->push(HiddenField::create('CropperX'));
-    		$fields->push(HiddenField::create('CropperY'));
-    		$fields->push(HiddenField::create('CropperWidth'));
-    		$fields->push(HiddenField::create('CropperHeight'));
+            $fields->push(HiddenField::create('ContainerY'));
+            $fields->push(HiddenField::create('ContainerWidth'));
+            $fields->push(HiddenField::create('ContainerHeight'));
+            $fields->push(HiddenField::create('CropperX'));
+            $fields->push(HiddenField::create('CropperY'));
+            $fields->push(HiddenField::create('CropperWidth'));
+            $fields->push(HiddenField::create('CropperHeight'));
         }
 
         return $fields;
--- js/salted-cropper.js
+++ js/salted-cropper.js
@@ -1,59 +1,59 @@
-(function($){
-	$.entwine('ss', function($) {
-		$('.salted-cropper').entwine({
-			onmatch: function(e) {
-				if ($(this).find('img').length > 0) {
+(function($)
+{
+    $.entwine('ss', function($)
+    {
+        $('.salted-cropper').entwine(
+        {
+            onmatch: function(e)
+            {
+                if ($(this).find('img').length > 0) {
 
-					var thisCropper = $(this),
-						thisForm = $(this).parents('form:eq(0)'),
-						image = $(this).find('img')[0],
-						ratio = $(this).attr('data-cropper-ratio'),
-						minWidth = $(this).attr('data-min-width'),
-						minHeight = $(this).attr('data-min-height'),
-						name = $(this).attr('data-name'),
-						cords = {
-							left: parseInt(thisForm.find('input[name="CropperX"]').val()),
-							top: parseInt(thisForm.find('input[name="CropperY"]').val()),
-							width: parseInt(thisForm.find('input[name="CropperWidth"]').val()),
-							height: parseInt(thisForm.find('input[name="CropperHeight"]').val())
-						};
+                    var thisCropper     =   $(this),
+                        thisForm        =   $(this).parents('form:eq(0)'),
+                        image           =   $(this).find('img')[0],
+                        ratio           =   $(this).attr('data-cropper-ratio'),
+                        minWidth        =   $(this).attr('data-min-width'),
+                        minHeight       =   $(this).attr('data-min-height'),
+                        name            =   $(this).attr('data-name'),
+                        cords           =   {
+                                                left: parseInt(thisForm.find('input[name="CropperX"]').val()),
+                                                top: parseInt(thisForm.find('input[name="CropperY"]').val()),
+                                                width: parseInt(thisForm.find('input[name="CropperWidth"]').val()),
+                                                height: parseInt(thisForm.find('input[name="CropperHeight"]').val())
+                                            },
+                        cropper         =   new Cropper(image,
+                                            {
+                                                viewMode: 3,
+                                                aspectRatio: ratio ? ratio : NaN,
+                                                zoomable: false,
+                                                minContainerWidth: minWidth,
+                                                minContainerHeight: minHeight,
+                                                crop: function(e) {
+                                                    var x = Math.round(cropper.getCanvasData().left * -1),
+                                                        y = Math.round(cropper.getCanvasData().top * -1),
+                                                        w = Math.round(cropper.getCanvasData().width),
+                                                        h = Math.round(cropper.getCanvasData().height),
+                                                        cx = Math.round(cropper.getCropBoxData().left),
+                                                        cy = Math.round(cropper.getCropBoxData().top),
+                                                        cw = Math.round(cropper.getCropBoxData().width),
+                                                        ch = Math.round(cropper.getCropBoxData().height);
 
-					// console.log(cords);
-					var cropper = new Cropper(image, {
-						viewMode: 3,
-						aspectRatio: ratio,
-						zoomable: false,
-						minContainerWidth: minWidth,
-						minContainerHeight: minHeight,
-						crop: function(e) {
-							var x = Math.round(cropper.getCanvasData().left * -1),
-								y = Math.round(cropper.getCanvasData().top * -1),
-								w = Math.round(cropper.getCanvasData().width),
-								h = Math.round(cropper.getCanvasData().height),
-								cx = Math.round(cropper.getCropBoxData().left),
-								cy = Math.round(cropper.getCropBoxData().top),
-								cw = Math.round(cropper.getCropBoxData().width),
-								ch = Math.round(cropper.getCropBoxData().height);
+                                                    thisForm.find('input[name="ContainerX"]').val(x);
+                                                    thisForm.find('input[name="ContainerY"]').val(y);
+                                                    thisForm.find('input[name="ContainerWidth"]').val(w);
+                                                    thisForm.find('input[name="ContainerHeight"]').val(h);
 
-							thisForm.find('input[name="ContainerX"]').val(x);
-							thisForm.find('input[name="ContainerY"]').val(y);
-							thisForm.find('input[name="ContainerWidth"]').val(w);
-							thisForm.find('input[name="ContainerHeight"]').val(h);
-
-							thisForm.find('input[name="CropperX"]').val(cx);
-							thisForm.find('input[name="CropperY"]').val(cy);
-							thisForm.find('input[name="CropperWidth"]').val(cw);
-							thisForm.find('input[name="CropperHeight"]').val(ch);
-						},
-						ready: function() {
-							cropper.setCropBoxData(cords);
-						}
-					});
-
-
-				}
-			}
-		});
-	});
-
+                                                    thisForm.find('input[name="CropperX"]').val(cx);
+                                                    thisForm.find('input[name="CropperY"]').val(cy);
+                                                    thisForm.find('input[name="CropperWidth"]').val(cw);
+                                                    thisForm.find('input[name="CropperHeight"]').val(ch);
+                                                },
+                                                ready: function() {
+                                                    cropper.setCropBoxData(cords);
+                                                }
+                                            });
+                }
+            }
+        });
+    });
 }(jQuery));
